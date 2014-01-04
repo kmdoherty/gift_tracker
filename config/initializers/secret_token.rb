4fe7d94656011e7a20fc8f2cabfe1f5591f63399
Kevin Doherty <kevin.doherty1@gmail.com>
2014-01-04 10:41:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftTracker::Application.config.secret_key_base = '591df4462364ab2843f445005c880194acdaba439ae42317106c41cf6cf8784bbf4375b5b39c0e37425a0fdf8453d30b2111af5e08398977c0fdef3758be1062'
